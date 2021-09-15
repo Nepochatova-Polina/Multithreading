582cd0656d900b85985613339b80aed197b061c2
Nepochatova Polina <nepochatova.polina@unicyb.kiev.ua>
2021-09-15 21:02:46 +0300

    added third part of lab Golang 2.0

---
+++ src/com/company/Lab2/ThreadLabC2.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+var waitGroup sync.WaitGroup
+var resultLineID int = 0
+
+type line struct {
+	id   int
+	line [50]int
+}
+
+func main() {
+	currentLineCh := make(chan line, 50)
+
+	var array [50]int
+	for i := 0; i < 50; i++ {
+		for j := 0; j < 50; j++ {
+			if i == 28 && j == 18 {
+				array[18] = 1
+			} else {
+				array[j] = 0
+			}
+		}
+		currentLineCh <- line{i, array}
+	}
+	for i := 1; i < 3; i++ {
+		waitGroup.Add(1)
+		go findValueInMatrix(currentLineCh)
+	}
+	waitGroup.Wait()
+}
+
+func findValueInMatrix(currentLineCh chan line) {
+	for resultLineID == 0 {
+		line := <-currentLineCh
+		for i := 0; i < 50; i++ {
+			if line.line[i] == 1 {
+				resultLineID = line.id
+				fmt.Println(line.id)
+				fmt.Println(line.line)
+				break
+			}
+		}
+	}
+	defer waitGroup.Done()
+	fmt.Println("routine finished")
+}
