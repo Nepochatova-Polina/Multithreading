208751f30d049933d99c176a073bfac950b81a83
Nepochatova Polina <nepochatova.polina@unicyb.kiev.ua>
2021-09-24 17:49:35 +0300

    added fourth lab package

---
+++ src/com/company/Lab4/ThreadLab3A.java
@@ -0,0 +1,15 @@
+package com.company.Lab4;
+
+
+import java.util.Arrays;
+import java.util.Random;
+
+
+public class ThreadLab3A {
+
+
+    public static void main(String[] args) {
+
+
+    }
+}
---
+++ src/com/company/Lab4/ThreadLab3B.java
@@ -0,0 +1,14 @@
+package com.company.Lab4;
+
+import java.util.Arrays;
+import java.util.Random;
+import java.util.concurrent.CountDownLatch;
+
+public class ThreadLab3B {
+
+    public static void main(String[] args) {
+
+
+    }
+
+}
